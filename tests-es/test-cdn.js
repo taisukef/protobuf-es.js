adb6d9fdc56fc57cda302659c4cbd9f465f81efd
Taisuke Fukuno <fukuno@jig.jp>
2021-05-03 08:02:16 +0900

    add demo using cdn

---
+++ tests-es/test-cdn.js
@@ -0,0 +1,18 @@
+import { protobuf } from "https://taisukef.github.io/protobuf-es.js/dist/protobuf-es.js";
+
+protobuf.load("awesome.proto", function(err, root) {
+  if (err)
+    throw err;
+
+  // example code
+  const AwesomeMessage = root.lookupType("awesomepackage.AwesomeMessage");
+
+  let message = AwesomeMessage.create({ awesomeField: "hello" });
+  console.log(`message = ${JSON.stringify(message)}`);
+
+  let buffer = AwesomeMessage.encode(message).finish();
+  console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
+
+  let decoded = AwesomeMessage.decode(buffer);
+  console.log(`decoded = ${JSON.stringify(decoded)}`);
+});
